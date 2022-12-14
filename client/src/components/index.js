1974f489073b6cdb5ff0bf1baaee1fec1151986d
Vitya <vitya199600@gmail.com>
2022-12-14 11:37:11 +0200

    created folders

---
+++ client/src/components/index.js
@@ -0,0 +1,21 @@
+
+import ListMoviesSection from "./ListMovies";
+import Movie from "./MovieCard/MovieCard";
+import Navigation from "./Navigation";
+import SelectedMoviesSection from "./SelectedMovies";
+import SelectedMoviesForm from './SelectedMoviesForm';
+import ConfirmModal from "./ConfirmModal";
+import SocialShare from "./SocialShare";
+import { Filters } from "./Filters";
+
+
+export {
+   Navigation,
+   Movie,
+   ListMoviesSection,
+   SelectedMoviesSection,
+   Filters,
+   SelectedMoviesForm,
+   ConfirmModal,
+   SocialShare,
+}
