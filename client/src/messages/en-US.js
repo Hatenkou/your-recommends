fa99e67522a22d08318f716bb7ace6bc2fdbd0e2
Vitya <vitya199600@gmail.com>
2022-12-14 12:11:22 +0200

    add hooks

---
+++ client/src/messages/en-US.js
@@ -0,0 +1,40 @@
+import { LOCALES } from '../const';
+export default {
+   [LOCALES.ENGLISH]: {
+      'navigation': {
+         'home': `Your recommendations`,
+         'settings': 'Settings'
+      },
+      'no_selected_movies': 'No selected movies',
+      'put_the_list_name': 'Put the list name',
+      'share_with_friends': 'Share with friends',
+      'copied': 'Copied!',
+      select: 'Select',
+      delete: 'Delete',
+      overview: 'Overview',
+      time: 'Min.',
+      likes: 'likes',
+      filters: {
+         sort_by: 'Sort by',
+         sort_direction: 'Sort direction',
+         include_adult: '18+',
+         year: 'Year',
+         release_year: 'Release year',
+         genre: 'Genre',
+         submit: 'Submit',
+         sort: {
+            'popularity': 'Popularity',
+            'release_date': 'Release date',
+            'revenue': 'Revenue',
+            'primary_release_date': 'Primary release date',
+            'original_title': 'Original title',
+            'vote_average': 'Vote average',
+            'vote_count': 'Vote count'
+         },
+         sort_direction_options: {
+            asc: 'ASC',
+            desc: 'DESC'
+         }
+      }
+   }
+};
